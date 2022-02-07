85d1a9088a9f02e968ab036f3cc59e1fd98b2c5d
SoongJr <SoongJr@googlemail.com>
2022-02-07 20:55:48 +0100

    fix incorrect conversion and extend to 8 switches. Any more than that can't be encoded in one analog signal... We might even decide to split the existing 8 switches and use two analog inputs for higher resolution. Have to test reliability.

--- DopamineBox.ino
+++ DopamineBox.ino
@@ -1,17 +1,11 @@
 /*
   Take input from analog pin and activate LEDs based on its value.
 
-  The idea is that the analog input is connected to the output of a voltage divider with 1M on one side
-  and multiple resistors in series on the other side.
-  Each of those resistors can be bypassed by a different switch, and each resistor's value is
-  chosen so that we can determine from the resulting voltage which of them have been bypassed and which haven't.
-  So the resistor for switch 0 is chosen so that it, and only it, brings the voltage over half the input voltage (e.g. 1M)
-  Then the resistor of switch 1 is chosen so that it, and only it, brings the voltage over half the remaining voltage (e.g. 500k)
-  Et cetera, with the caveat that analog values are not accurate, and we also don't have every value of resistor at hand,
-  so the resistor values were chosen differently, see the macro definition sector.
+  The state of all switches (active/pressed or not, 1 or 0) is "encoded" into a single analog signal
+  using an R-2R resistor ladder.
 
   The circuit:
-  - analog input A0 is the output of abovementioned voltage divider
+  - analog input A0 is the output of above-mentioned resistor ladder
   - digital outputs used to activate LEDs (see macro definition section for details)
 
   created 07 Feb 2022
@@ -24,14 +18,26 @@
 
 #define DEBUG_MODE
 
+// Pins
 #define INPUT_PIN A0
-#define SWITCH0_R 1000000 // 1 mega-Ohm
-#define SWITCH1_R 470000  // 470 kilo-Ohm
-// voltages for each switch are calculated via the voltage divider formula:
-#define SWITCH0_VOLT (1024 * (1000000+SWITCH0_R)/SWITCH0_R))
-#define SWITCH1_VOLT (1024 * (1000000+SWITCH1_R)/SWITCH1_R))
 #define LED0 2
 #define LED1 3
+#define LED2 4
+#define LED3 5
+#define LED4 6
+#define LED5 7
+#define LED6 8
+#define LED7 9
+// signal levels for each switch:
+// (switch 7 results in a difference of only 4 in the analog signal, that's probably already below the noise level and any more would be a waste)
+#define SWITCH0_SIGNAL (1024 / (1 << 1))
+#define SWITCH1_SIGNAL (1024 / (1 << 2))
+#define SWITCH2_SIGNAL (1024 / (1 << 3))
+#define SWITCH3_SIGNAL (1024 / (1 << 4))
+#define SWITCH4_SIGNAL (1024 / (1 << 5))
+#define SWITCH5_SIGNAL (1024 / (1 << 6))
+#define SWITCH6_SIGNAL (1024 / (1 << 7))
+#define SWITCH7_SIGNAL (1024 / (1 << 8))
 
 void setup()
 {
@@ -44,9 +50,21 @@ void setup()
   // configure digital pins for LEDs as output
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
   // explicitly disable all LEDs
   digitalWrite(LED0, LOW);
   digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  digitalWrite(LED7, LOW);
 }
 
 // checkSwitch compares the switch-specific value with the input values,
@@ -55,16 +73,25 @@ void checkSwitch(int &inputVal, int switchValue, uint8_t led)
 {
 #ifdef DEBUG_MODE
   // print out the measured value
+  Serial.print("switch ");
+  Serial.print(led - LED0);
+  Serial.print(": ");
   Serial.print(inputVal);
+  Serial.print(" > ");
+  Serial.print(switchValue);
+  Serial.println("?");
 #endif
-  // check if the voltage is over the expected value for this switch (with a small margin for error)
-  if (inputVal >= switchValue - 8)
+  // check if the signal exceeds the expected value for this switch (with a small margin for error)
+  if (inputVal > switchValue - 8)
   {
+    // switch-specific voltage detected, meaning the switch is pressed and we activate the LED
     digitalWrite(led, HIGH);
-    inputVal -= switchValue; // deduct the voltage caused by switch 0 fromt eh input value for further examination
+    // also deduct this value from the input value for further examination
+    inputVal -= switchValue;
   }
   else
   {
+    // deactivate the LED
     digitalWrite(led, LOW);
   }
 }
@@ -73,13 +100,17 @@ void loop()
 {
 #ifdef DEBUG_MODE
   delay(1000); // delay to make serial output readable
+  Serial.println("---");
 #endif
   // read the analog value into a variable
   int inputVal = analogRead(INPUT_PIN);
   // from the analog value, determine which switches had been pressed
-  // (each of them bypasses one identifiable part of the voltage divider circuit, resulting in a voltage between 0V and 2.5V or 0-512)
-  // e.g.: switch 0 is responsible for half the possible voltage (so half of 512), and so are all other switches combined.
-  //       If it is activated, the voltage cannot be over 1.25V.
-  checkSwitch(inputVal, SWITCH0_VOLT, LED0);
-  checkSwitch(inputVal, SWITCH1_VOLT, LED1);
+  checkSwitch(inputVal, SWITCH0_SIGNAL, LED0);
+  checkSwitch(inputVal, SWITCH1_SIGNAL, LED1);
+  checkSwitch(inputVal, SWITCH2_SIGNAL, LED2);
+  checkSwitch(inputVal, SWITCH3_SIGNAL, LED3);
+  checkSwitch(inputVal, SWITCH4_SIGNAL, LED4);
+  checkSwitch(inputVal, SWITCH5_SIGNAL, LED5);
+  checkSwitch(inputVal, SWITCH6_SIGNAL, LED6);
+  checkSwitch(inputVal, SWITCH7_SIGNAL, LED7);
 }
